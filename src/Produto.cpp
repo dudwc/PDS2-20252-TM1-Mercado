e1800491e81df615e98c0e284aecfc8dfd496d06
duda c <dudacarvalhocosta@gmail.com>
2025-10-03 11:26:45 -0300

    Update Produto.cpp

--- src/Produto.cpp
+++ src/Produto.cpp
@@ -20,6 +20,26 @@ bool Produto::estaDisponivel() const{
     return disponivel;
 }
 
+void Produto::setName(std::string novoNome){
+    nome = novoNome;
+}
+
+void Produto::setMarca(std::string novaMarca){
+    marca = novaMarca;
+}
+
+void Produto::setPreco(double novoPreco){
+    preco = novoPreco;
+}
+
+void Produto::setID(int novoID){
+    id = novoID;
+}
+
+void Produto::setDisponivel(bool disponivel){
+    this->disponivel = disponivel;
+}
+
 void Produto::alterarPreco(){
     std::cout << "Preco atual: " << preco << std::endl;
     double novoPreco;
