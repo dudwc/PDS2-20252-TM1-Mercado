857e4b3a804a14d14c3ab428db60623726d5c4c1
MaisaRamos <130862310+mais4ram0s@users.noreply.github.com>
2025-10-03 17:57:59 -0300

    Update Caixa.hpp

--- include/Caixa.hpp
+++ include/Caixa.hpp
@@ -1,1 +1,28 @@
+#ifndef CAIXA_H
+#define CAIXA_H
 
+#include <iostream>
+#include <string>
+#include "Produto.hpp"
+
+class Caixa{
+    private:
+        std::string formaPagamento;
+        int usuario;
+        int valorTotal;
+        std::vector <Produto> itensVenda;
+    
+    public:
+        Caixa(std::string formaPagamento, int usuario, int valorTotal, std::vector <Produto> itensVenda) :
+        formaPagamento(formaPagamento), usuario(usuario), valorTotal(valorTotal), itensVenda(itensVenda) {}
+        ~Caixa(){};
+
+        void RegistrarProduto(Produto p);
+        bool RemoverItem(Produto p);
+        int ExibirTotal() const;
+        std::string  getFormaPagamento() const;
+        int getUsuario() const;
+};
+
+
+#endif
