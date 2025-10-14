89bf6b96be7043625f5a50ff57efc6172550806c
gpradotxt <guilhermeprado245@gmail.com>
2025-10-14 16:19:41 -0300

    Add files via upload

--- include/Estoque.hpp
+++ include/Estoque.hpp
@@ -1,1 +1,26 @@
-//
+#ifndef ESTOQUE_H
+#define ESTOQUE_H
+
+#include <iostream>
+#include <map>
+#include <fstream>
+#include "Produto.hpp"
+
+class Estoque{
+    private:
+        std::map<Produto, int> produtos;
+        std::fstream arquivo;
+    public:
+        Estoque() {};
+        Estoque(const std::map<Produto, int>& produtos) : 
+            produtos(produtos) {}
+        ~Estoque() {};
+
+        void adicionarProduto(const Produto& produto, int qtd);
+        bool removerProduto(int id, int qtd);
+        const Produto* buscarProduto(int id);
+        void listarProdutos() const;
+};
+
+
+#endif
