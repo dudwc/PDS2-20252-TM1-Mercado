bb0bc9c163cd928fb594db6bbb87c74aac3d8db5
Larissa Vieira <larissavsilva183@gmail.com>
2025-10-16 16:25:21 -0300

    Refactor Cliente class to include CPF and purchase history
    Removed unused member variables and methods, added CPF and purchase history.

--- include/Cliente.hpp
+++ include/Cliente.hpp
@@ -4,29 +4,26 @@
 #include <string>
 #include <vector>
 #include "Produto.hpp"
-#include "Caixa.hpp"
-#include "Estoque.hpp"
+
 
 class Cliente{
 
 private:
-    int id;
     std::string nome;
-    std::string formaPagamento;
-    std::vector<Produto> carrinho;
+    std::string cpf;
+    std::vector<Produto> historicoCompras;
 
-    public:
-        Cliente(int id, std::string nome);
 
-        void escolherProduto(Estoque& estoque, int idProduto, int quantidade);
-        void verificarPrecos(const Estoque& estoque) const;
-        void escolherFormaPagamento(std::string forma);
-        void finalizarCompra(Caixa& caixa);
-        void receberAvisoPromocao(const std::string& mensagem);
+public:
+    Cliente(std::string nome = "", std::string cpf = "");
+    
+    std::string getNome() const;
+    std::string getCPF() const;
 
-        int getId() const;
-        std::string getNome() const;
-        std::string getFormaPagamento() const;
+    void adicionarCompra(const Produto& produto);
+    void mostrarHistorico() const;
+    void salvarEmAquivo();
+    void carregarDeArquivo();
 
 };
 
