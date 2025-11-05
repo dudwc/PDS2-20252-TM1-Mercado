a58620d7ee9d2bcb7d269d5c3ce551bb6192ef9d
MaisaRamos <130862310+mais4ram0s@users.noreply.github.com>
2025-11-04 22:54:00 -0300

    Update Caixa.hpp e adição de duas funções
    Novas funções:
    -ExibirCarrinho;
    -GerarNotaFiscal.

--- include/Caixa.hpp
+++ include/Caixa.hpp
@@ -12,25 +12,26 @@ class Caixa{
     private:
         std::string formaPagamento;
         int usuario;
-        Estoque& estoque;
+        Estoque& estoque;//agregação, ref ao estoque compartilhado
         
-        //carrinho: chave=ID do produto e valor=quantidade
-        std::map<int, int> carrinho;
+        //carrinho: chave=ID do produto e valor=quantd
+        std::map<int, double> carrinho;
     
     public:
         Caixa(std::string formaPagamento, int usuario, Estoque& estoque) :
         formaPagamento(formaPagamento), usuario(usuario), estoque(estoque) {}
         ~Caixa(){};
-
-        
+        //registrar produto no estoque
         bool CadastrarProduto(const Produto& p, int quantidade);
 
-        
-        bool AdicionarItem(const Produto& p,double quantidade=1);//quantidade=1 ao passar somente o p como parametro, já entende que a quantidade é um
+        //métodos de carrinho
+        bool AdicionarItem(const Produto& p,double quantidade=1);//quantidade=1 ao passar somente o p como parametro, já entende que a qntd é um
         bool RemoverItem(const Produto& p,double quantidade=1);
-        int ExibirTotal() const;
+        double ExibirTotal() const;
+        void ExibirCarrinho() const;
+        void GerarNotaFiscal() const;
 
-        
+        //métodos para acesso
         std::string  getFormaPagamento() const;
         int getUsuario() const;
 };
