9ebc25738faf44ebda0523c698bb40d1e8c4ce64
MaisaRamos <130862310+mais4ram0s@users.noreply.github.com>
2025-11-05 19:48:59 -0300

    Correção do construtor
    Parâmetro formaPagamento pode ser inserido quando necessário.

--- include/Caixa.hpp
+++ include/Caixa.hpp
@@ -12,26 +12,25 @@ class Caixa{
     private:
         std::string formaPagamento;
         int usuario;
-        Estoque& estoque;//agregação, ref ao estoque compartilhado
+        Estoque& estoque;
         
         //carrinho: chave=ID do produto e valor=quantd
         std::map<int, double> carrinho;
     
     public:
-        Caixa(std::string formaPagamento, int usuario, Estoque& estoque) :
-        formaPagamento(formaPagamento), usuario(usuario), estoque(estoque) {}
+        Caixa(int usuario, Estoque &estoque, std::string formaPagamento = "");
         ~Caixa(){};
-        //registrar produto no estoque
+
         bool CadastrarProduto(const Produto& p, int quantidade);
 
-        //métodos de carrinho
-        bool AdicionarItem(const Produto& p,double quantidade=1);//quantidade=1 ao passar somente o p como parametro, já entende que a qntd é um
+        
+        bool AdicionarItem(const Produto& p,double quantidade=1);
         bool RemoverItem(const Produto& p,double quantidade=1);
         double ExibirTotal() const;
         void ExibirCarrinho() const;
         void GerarNotaFiscal() const;
 
-        //métodos para acesso
+        
         std::string  getFormaPagamento() const;
         int getUsuario() const;
 };
