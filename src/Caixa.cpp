1413f731c06dd0ff8cbc89e3b2b4580be46651ba
MaisaRamos <130862310+mais4ram0s@users.noreply.github.com>
2025-11-05 19:50:46 -0300

    Update Caixa.cpp
    Mudança no construtor

--- src/Caixa.cpp
+++ src/Caixa.cpp
@@ -6,7 +6,10 @@
 #include <map>
 #include <iomanip>
 
-Caixa::Caixa(std::string formaPagamento, int usuario, Estoque &estoque) : formaPagamento(formaPagamento), usuario(usuario), estoque(estoque) {}
+
+Caixa::Caixa(int usuario, Estoque &estoque, std::string formaPagamento)
+    : formaPagamento(formaPagamento), usuario(usuario), estoque(estoque) {}
+
 
 std::string Caixa::getFormaPagamento() const{
         return formaPagamento;
@@ -33,7 +36,7 @@ bool Caixa::AdicionarItem(const Produto &p, double quantidade){
                 std::cout << "Quantidade inválida.\n";
                 return false;
         }
-        if (carrinho.contains(p.getID())){ // verifica se já tem outras unidades no carrinho
+        if (carrinho.find(p.getID()) != carrinho.end()){ 
                 carrinho[p.getID()] += quantidade;
         }else{
                 carrinho[p.getID()] = quantidade;
@@ -41,7 +44,7 @@ bool Caixa::AdicionarItem(const Produto &p, double quantidade){
         return true;
 }
 bool Caixa::RemoverItem(const Produto &p, double quantidade){
-        if (!carrinho.contains(p.getID())){
+        if (carrinho.find(p.getID()) == carrinho.end()){
                 return false;
         }
         if (quantidade >= carrinho[p.getID()]){
@@ -54,8 +57,8 @@ bool Caixa::RemoverItem(const Produto &p, double quantidade){
 double Caixa::ExibirTotal() const{
         double total = 0.0;
         for (auto it = carrinho.begin(); it != carrinho.end(); it++){
-                int chave = it->first;          // chave representa o ID
-                double quantidade = it->second; // quantidade representa o num de unidades no carrinho
+                int chave = it->first;         
+                double quantidade = it->second; 
                 const Produto *p = estoque.buscarID(chave);
                 if (p != nullptr){
                         total += p->getPreco() * quantidade;
@@ -91,14 +94,15 @@ void Caixa::GerarNotaFiscal() const{
         std::cout << "-----------------------------------------\n";
         for (const auto &par : carrinho){
                 int codigo = par.first;
-                double quantidade = par.second;buscarID(codigo);
-                if (p){
+                double quantidade = par.second; 
+                const Produto* p = estoque.buscarID(codigo);
+                        if (p != nullptr) {        
                         std::string nome = p->getName();
                         double preco = p->getPreco();
                         std::cout << std::left << std::setw(20) << nome << std::setw(10) << quantidade
                                   << "R$ " << std::fixed << std::setprecision(2)
                                   << preco * quantidade << std::endl;
-                }
+                        }
         }
         std::cout << "-----------------------------------------\n";
         std::cout << std::setw(30) << "TOTAL:"
