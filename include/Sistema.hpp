91e1ec4513b47b05bb333e4fcd328d1c3d77da80
gpradotxt <guilhermeprado245@gmail.com>
2025-11-19 00:02:44 -0300

    Criação de subclasses de sistema

--- include/Sistema.hpp
+++ include/Sistema.hpp
@@ -14,24 +14,53 @@
 #include "Cliente.hpp"
 
 class Sistema {
-    private:
+    protected:
         Estoque estoque;
         std::fstream arquivo;
-        std::vector<Funcionario> funcionarios;
-        static int proximoIDFuncionario;  // Rastreia o próximo ID a ser atribuído
     
     public:
     Sistema();
     ~Sistema();
 
-        void iniciar();
-        void exibirMenuPrincipal();
+        virtual void iniciar();
+        virtual void exibirMenu();
         void exibirLogo();
         void print(std::string texto);
+        void carregar();
+        
+};
+
+class SistemaFuncionario : public Sistema {
+    private:
+        std::vector<Funcionario> funcionarios;
+        static int proximoIDFuncionario;  // Rastreia o próximo ID a ser atribuído
+    
+    public:
+        SistemaFuncionario();
+        ~SistemaFuncionario();
+
+        void iniciar() override;
+        void exibirMenu() override;
         Funcionario* cadastrarFuncionario();
         void loginFuncionario();
-        bool verificarLogin(const std::string & usuario, const int id, const std::string& senha);
+        bool verificarLogin(const int id, const std::string& senha);
         void carregarProximoID();
         void salvarProximoID();
 };
+
+class SistemaCliente : public Sistema {
+    private:
+        //Caixa caixa;        
+
+    public:
+        SistemaCliente();
+        ~SistemaCliente();
+
+        void iniciar() override;
+        void exibirMenu() override;
+        Cliente* cadastrarCliente();
+        void loginCliente();
+};
+
+
 #endif
