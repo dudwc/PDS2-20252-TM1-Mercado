7bee9b660e10a23b1c7dbe31da5e3e11dca5cd77
MaisaRamos <130862310+mais4ram0s@users.noreply.github.com>
2025-11-20 13:22:09 -0300

    Update Estoque.hpp

--- include/Estoque.hpp
+++ include/Estoque.hpp
@@ -8,18 +8,19 @@
 
 class Estoque{
     private:
-        std::map<Produto, int> produtos;
+        std::map<Produto, double> produtos;
         std::fstream arquivo;
     public:
         Estoque();
-        Estoque(const std::map<Produto, int>& produtos);
+        Estoque(const std::map<Produto, double>& produtos);
         ~Estoque() {};
 
-        void adicionarProduto(const Produto& produto, int qtd);
-        bool removerProduto(int id, int qtd);
+        void adicionarProduto(const Produto& produto, double qtd);
+        bool removerProduto(int id, double qtd);
         const Produto* buscarID(int id);
         const Produto* buscarNome(const std::string& nome);
         void listarProdutos() const;
+        double getQuantidade(int id) const;
 };
 
 
